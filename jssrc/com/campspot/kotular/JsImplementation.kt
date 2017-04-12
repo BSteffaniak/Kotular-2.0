eb44dae214d6335fb6bcabfdda01df6926a4ed94
Braden Steffaniak <BradenSteffaniak@gmail.com>
2017-04-12 11:58:18 -0600

    Added JsImplementation file

--- jssrc/com/campspot/kotular/JsImplementation.kt
+++ jssrc/com/campspot/kotular/JsImplementation.kt
@@ -1,10 +1,22 @@
-package com.campspot.kotular.angular
-
-import com.campspot.kotular.js.JsArray
+package com.campspot.kotular
 
 /**
- * Created by bradensteffaniak on 4/11/17.
+ * Created by bradensteffaniak on 4/12/17.
  */
+
+@JsName("Array")
+external interface JsArray<T> {
+    fun push(obj: T): T
+    fun splice(i1: Int, i2: Int): JsArray<T>
+    fun indexOf(obj: T): Int
+    fun forEach(action: (T) -> Unit)
+    fun <X> map(action: (T) -> X): JsArray<X>
+    fun filter(action: (T) -> Boolean): JsArray<T>
+    @JsName("length") val size: Int
+}
+
+@JsName("Array")
+external fun <X> JsArray(vararg x: Any): JsArray<X>
 
 external interface Angular {
     fun module(name: String, deps: JsArray<String>): Module
@@ -59,3 +71,5 @@ external interface LocalStorage {
 
 external val angular: Angular
 @JsName("this") external val controllerScope: Scope
+
+external val localStorage: LocalStorage
